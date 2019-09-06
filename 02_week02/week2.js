2baa7f79aa7a18981b7898383bbf1ea024ae2ed9
EC2 Default User <ec2-user@ip-172-31-44-209.us-east-2.compute.internal>
2019-09-06 21:18:41 +0000

    week

---
+++ 02_week02/week2.js
@@ -0,0 +1,27 @@
+//strategy 1 : target always the first td of a tr
+//strategy 2: target the horizontal line and select elements from there
+
+
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+// load the thesis text file into a variable, `content`
+// this is the file that we created in the starter code from last week
+var content = fs.readFileSync('/home/ec2-user/environment/data-structures/01_week01/data/06.txt');
+
+// load `content` into a cheerio object
+var $ = cheerio.load(content);
+
+// print (to the console) adresses
+$('tr tr tr').each(function(i, elem) {
+    console.log($(elem).children().first().html().trim());
+});
+
+// write the adresses to a text file
+var result = ''; // this variable will hold the lines of text
+
+$('tr tr tr').each(function(i, elem) {
+    result += ($(elem).children().first()).html().html().removeAttr('a').trim() + '\n';
+});
+
+fs.writeFileSync('new.txt', result);
