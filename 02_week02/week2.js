072f7387ade5cd5b1eae79cc4e21d36185db43d4
EC2 Default User <ec2-user@ip-172-31-44-209.us-east-2.compute.internal>
2019-09-06 21:46:10 +0000

    week02-base

--- 02_week02/week2.js
+++ 02_week02/week2.js
@@ -12,16 +12,17 @@ var content = fs.readFileSync('/home/ec2-user/environment/data-structures/01_wee
 // load `content` into a cheerio object
 var $ = cheerio.load(content);
 
+
 // print (to the console) adresses
-$('tr tr tr').each(function(i, elem) {
-    console.log($(elem).children().first().html().trim());
+    $('tr tr tr').each(function(i, elem) {
+                 console.log($(elem).children().first().text().trim());
 });
 
 // write the adresses to a text file
 var result = ''; // this variable will hold the lines of text
 
 $('tr tr tr').each(function(i, elem) {
-    result += ($(elem).children().first()).html().html().removeAttr('a').trim() + '\n';
+    result += ($(elem).children().first().text().trim()) + '\n';
 });
 
 fs.writeFileSync('new.txt', result);
