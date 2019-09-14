8792a6bb90672e8f7a66b8863aace970761ae0da
EC2 Default User <ec2-user@ip-172-31-44-209.us-east-2.compute.internal>
2019-09-14 20:39:29 +0000

    stuck

--- 03_week03/week03.js
+++ 03_week03/week03.js
@@ -18,45 +18,28 @@ var addresses = [];
 //pull adress out of file, just the street
 
 let content_f = fs.readFileSync('/home/ec2-user/environment/data-structures/01_week01/data/06.txt');
-
 const $ = cheerio.load(content_f);
-
 var meetingData = [];
-
 
 
  $('td').each(function(i, elem) {
      if($(elem).attr("style")=="border-bottom:1px solid #e3e3e3; width:260px"){
          
-         var thisMeeting = {};
-         thisMeeting.streetAdress = $(elem).html().split('<br>')[2].trim().split(',')[0];
-         thisMeeting.city = "NewYork";
-         thisMeeting.state = "NY";
-         meetingData.push(thisMeeting);
+        //  var thisMeeting = {};
+        //  thisMeeting.streetAdress = $(elem).html().split('<br>')[2].trim().split(',')[0];
+        //  thisMeeting.city = "NewYork";
+        //  thisMeeting.state = "NY";
+        //  meetingData.push(thisMeeting);
          
-         var zwischenlager = $(elem).html().split('<br>')[2].trim().split(',')[0];
-         addresses.push(zwischenlager);
+         var parsed = $(elem).html().split('<br>')[2].trim().split(',')[0];
+         JSON.stringify(parsed);
+         addresses.push(parsed);
         // console.log(zwischenlager);
-
      }
  });
- 
         console.log(addresses);
-// console.log(meetingData);
 
-// var zwischenlager = $(elem).html().split('<br>')[2].trim().split(',')[0];
-
-// console.log(zwischenlager);
-
-
-
-
-
-fs.writeFileSync('adresses_06.json', JSON.stringify(meetingData));
-
-
-// eachSeries in the async module iterates over an array and operates on each item in the array in series
-async.eachSeries(addresses, function(value, callback) {
+async.eachSeries(addresses[1], function(value, callback) {
     var apiRequest = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?';
     apiRequest += 'streetAddress=' + value.split(' ').join('%20');
     apiRequest += '&city=New%20York&state=NY&apikey=' + apiKey;
@@ -72,8 +55,58 @@ async.eachSeries(addresses, function(value, callback) {
     });
     setTimeout(callback, 2000);
 }, function() {
-    fs.writeFileSync('data/first.json', JSON.stringify(allData));
+    fs.writeFileSync('geo_06.json', JSON.stringify(allData));
     console.log('*** *** *** *** ***');
     console.log('Number of meetings in this zone: ');
-    console.log(meetingsData.length);
+    console.log(allData.length);
 });
+
+
+
+
+
+
+
+// // dependencies
+// var request = require('request'); // npm install request
+// var async = require('async'); // npm install async
+// var fs = require('fs');
+// const dotenv = require('dotenv'); // npm install dotenv
+
+// // TAMU api key
+// dotenv.config();
+// const apiKey = process.env.TAMU_KEY;
+
+// // geocode addresses
+// var meetingsData = [];
+// var source = fs.readFileSync('adresses_06.json');
+// source = JSON.parse(source);
+
+// var addresses = [source['streetAdress']];
+// console.log(addresses);
+
+
+
+
+// // eachSeries in the async module iterates over an array and operates on each item in the array in series
+// async.eachSeries(addresses, function(value, callback) {
+//     var apiRequest = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?';
+//     apiRequest += 'streetAddress=' + value.split(' ').join('%20');
+//     apiRequest += '&city=New%20York&state=NY&apikey=' + apiKey;
+//     apiRequest += '&format=json&version=4.01';
+    
+//     request(apiRequest, function(err, resp, body) {
+//         if (err) {throw err;}
+//         else {
+//             var tamuGeo = JSON.parse(body);
+//             console.log(tamuGeo['FeatureMatchingResultType']);
+//             meetingsData.push(tamuGeo);
+//         }
+//     });
+//     setTimeout(callback, 2000);
+// }, function() {
+//     fs.writeFileSync('geo_06.json', JSON.stringify(meetingsData));
+//     console.log('*** *** *** *** ***');
+//     console.log('Number of meetings in this zone: ');
+//     console.log(meetingsData.length);
+// });
